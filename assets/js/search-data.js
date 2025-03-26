7f356674dbf36964a8653768f81ffebcb1b45e33
ustc-hyin <yinhnavi@mail.ustc.edu.cn>
2025-03-26 12:24:45 +0000

    Deploying to gh-pages from @ ustc-hyin/ustc-hyin.github.io@029f2a1dc5808cc9b78c56fe152f347a4a5ba541 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,18 +451,18 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/ustc-hyin", "_blank");
+        },
+      },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -472,11 +472,11 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/ustc_hyin", "_blank");
         },
       },{
       id: 'light-theme',
